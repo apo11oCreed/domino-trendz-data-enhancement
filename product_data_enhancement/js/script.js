717e7aebaf66f2025ddbbc6d3003a88a9703eaa9
nc <nelson.ncdesigns@gmail.com>
2021-04-18 00:27:45 -0400

    App working as expected. The output json will require additional preparation before converting to CSV

--- product_data_enhancement/js/script.js
+++ product_data_enhancement/js/script.js
@@ -69,133 +69,149 @@ function necromancer(data){
 
             $.each(collectionsArray,function(collections){
 
-                var price;
+                var price,
+                oneSizeFitsMost;
 
                 switch (collectionsArray[collections]) {
                     case 'Scrub Cap':
                         price = '20.0';
+                        oneSizeFitsMost=' One size fits most.';
+                        break;
+                    case 'Scrub Caps':
+                        price = '20.0';
+                        oneSizeFitsMost=' One size fits most.';
                         break;
                     case 'Zippy Clutch':
                         price = '40.0';
                         break;
                     case 'Begonia Bag':
                         price = '60.0';
+                        break;
+                    case 'Bouffant Scrub Cap':
+                        oneSizeFitsMost=' One size fits most.';
+                        break;
+                    case 'Long Hair Scrub Cap':
+                        oneSizeFitsMost=' One size fits most.';
+                        break;
                     default:
                         price=data[item].price;
+                        oneSizeFitsMost='';
                         break;
                 }
 
                 if(collections!=0){
 
                     var newProduct = new Object();
-                    newProduct.additionalInfoDescription1=data[item].additionalInfoDescription1;
-                    newProduct.additionalInfoDescription2=data[item].additionalInfoDescription2;
-                    newProduct.additionalInfoDescription3=data[item].additionalInfoDescription3;
-                    newProduct.additionalInfoDescription4=data[item].additionalInfoDescription4;
-                    newProduct.additionalInfoDescription5=data[item].additionalInfoDescription5;
-                    newProduct.additionalInfoDescription6=data[item].additionalInfoDescription6;
-                    newProduct.additionalInfoTitle1=data[item].additionalInfoTitle1;
-                    newProduct.additionalInfoTitle2=data[item].additionalInfoTitle2;
-                    newProduct.additionalInfoTitle3=data[item].additionalInfoTitle3;
-                    newProduct.additionalInfoTitle4=data[item].additionalInfoTitle4;
-                    newProduct.additionalInfoTitle5=data[item].additionalInfoTitle5;
-                    newProduct.additionalInfoTitle6=data[item].additionalInfoTitle6;
-                    newProduct.collections=collectionsArray[collections];
-                    newProduct.customTextCharLimit1=data[item].customTextCharLimit1;
-                    newProduct.customTextCharLimit2=data[item].customTextCharLimit2;
-                    newProduct.customTextField1=data[item].customTextField1;
-                    newProduct.customTextField2=data[item].customTextField2;
-                    newProduct.customTextMandatory1=data[item].customTextMandatory1;
-                    newProduct.customTextMandatory2=data[item].customTextMandatory2;
-                    newProduct.description=data[item].description;
-                    newProduct.discountMode=data[item].discountMode;
-                    newProduct.discountValue=data[item].discountValue;
+
+                    newProduct.handleId=handleId + counter++;
                     newProduct.fieldType=data[item].fieldType;
-                    newProduct.handleIds=handleId + counter++;
-                    newProduct.inventory=data[item].inventory;
                     newProduct.name=data[item].name + ' ' + collectionsArray[collections];
-                    newProduct.price=price;
+                    newProduct.description=productSizeObj[collectionsArray[collections]]=="One Size fits most" ? data[item].description + " One Size fits most.":data[item].description;
                     newProduct.productImageUrl=data[item].productImageUrl;
-                    newProduct.productOptionDescription1=productSizeObj[collectionsArray[collections]];
-                    newProduct.productOptionDescription2='trash';
-                    newProduct.productOptionDescription3='trash';
-                    newProduct.productOptionDescription4='trash';
-                    newProduct.productOptionDescription5='trash';
-                    newProduct.productOptionDescription6='trash';
-                    newProduct.productOptionName1='Sizes';
-                    newProduct.productOptionName2='trash';
-                    newProduct.productOptionName3='trash';
-                    newProduct.productOptionName4='trash';
-                    newProduct.productOptionName5='trash';
-                    newProduct.productOptionName6='trash';
-                    newProduct.productOptionType1='DROP_DOWN';
-                    newProduct.productOptionType2='trash';
-                    newProduct.productOptionType3='trash';
-                    newProduct.productOptionType4='trash';
-                    newProduct.productOptionType5='trash';
-                    newProduct.productOptionType6='trash';
-                    newProduct.ribbon=data[item].ribbon;
+                    newProduct.collections=collectionsArray[collections];
                     newProduct.sku=data[item].sku;
+                    newProduct.ribbon=data[item].ribbon;
+                    newProduct.price=data[item].price;
                     newProduct.surcharge=data[item].surcharge;
                     newProduct.visible=data[item].visible;
+                    newProduct.discountMode=data[item].discountMode;
+                    newProduct.discountValue=data[item].discountValue;
+                    newProduct.inventory=data[item].inventory;
                     newProduct.weight=data[item].weight;
+                    newProduct.productOptionName1=productSizeObj[collectionsArray[collections]]=="One Size fits most" || collectionsArray[collections]=='Scrub Cap' || collectionsArray[collections]=='Scrub Caps' || collectionsArray[collections]=='Bouffant Scrub Cap'|| collectionsArray[collections]=='Long Hair Scrub Cap'?'trash':'Sizes';
+                    newProduct.productOptionType1=productSizeObj[collectionsArray[collections]]=="One Size fits most" || collectionsArray[collections]=='Scrub Cap' || collectionsArray[collections]=='Scrub Caps' || collectionsArray[collections]=='Bouffant Scrub Cap'|| collectionsArray[collections]=='Long Hair Scrub Cap'?'trash':'DROP_DOWN';
+                    newProduct.productOptionDescription1=productSizeObj[collectionsArray[collections]]=="One Size fits most"?'trash':productSizeObj[collectionsArray[collections]];
+                    newProduct.productOptionName2="trash";
+                    newProduct.productOptionType2="trash";
+                    newProduct.productOptionDescription2="trash";
+                    newProduct.productOptionName3="trash";
+                    newProduct.productOptionType3="trash";
+                    newProduct.productOptionDescription3="trash";
+                    newProduct.productOptionName4="trash";
+                    newProduct.productOptionType4="trash";
+                    newProduct.productOptionDescription4="trash";
+                    newProduct.productOptionName5="trash";
+                    newProduct.productOptionType5="trash";
+                    newProduct.productOptionDescription5="trash";
+                    newProduct.productOptionName6="trash";
+                    newProduct.productOptionType6="trash";
+                    newProduct.productOptionDescription6="trash";
+                    newProduct.additionalInfoTitle1="trash";
+                    newProduct.additionalInfoDescription1="trash";
+                    newProduct.additionalInfoTitle2="trash";
+                    newProduct.additionalInfoDescription2="trash";
+                    newProduct.additionalInfoTitle3="trash";
+                    newProduct.additionalInfoDescription3="trash";
+                    newProduct.additionalInfoTitle4="trash";
+                    newProduct.additionalInfoDescription4="trash";
+                    newProduct.additionalInfoTitle5="trash";
+                    newProduct.additionalInfoDescription5="trash";
+                    newProduct.additionalInfoTitle6="trash";
+                    newProduct.additionalInfoDescription6="trash";
+                    newProduct.customTextField1=data[item].customTextField1;
+                    newProduct.customTextCharLimit1=data[item].customTextCharLimit1;
+                    newProduct.customTextMandatory1=data[item].customTextMandatory1;
+                    newProduct.customTextField2=data[item].customTextField2;
+                    newProduct.customTextCharLimit2=data[item].customTextCharLimit2;
+                    newProduct.customTextMandatory2=data[item].customTextMandatory2;
 
                     newData.push(newProduct);
                     
                 } else {
 
                     var newProduct = new Object();
-                    newProduct.additionalInfoDescription1=data[item].additionalInfoDescription1;
-                    newProduct.additionalInfoDescription2=data[item].additionalInfoDescription2;
-                    newProduct.additionalInfoDescription3=data[item].additionalInfoDescription3;
-                    newProduct.additionalInfoDescription4=data[item].additionalInfoDescription4;
-                    newProduct.additionalInfoDescription5=data[item].additionalInfoDescription5;
-                    newProduct.additionalInfoDescription6=data[item].additionalInfoDescription6;
-                    newProduct.additionalInfoTitle1=data[item].additionalInfoTitle1;
-                    newProduct.additionalInfoTitle2=data[item].additionalInfoTitle2;
-                    newProduct.additionalInfoTitle3=data[item].additionalInfoTitle3;
-                    newProduct.additionalInfoTitle4=data[item].additionalInfoTitle4;
-                    newProduct.additionalInfoTitle5=data[item].additionalInfoTitle5;
-                    newProduct.additionalInfoTitle6=data[item].additionalInfoTitle6;
-                    newProduct.collections=collectionsArray[0];
-                    newProduct.customTextCharLimit1=data[item].customTextCharLimit1;
-                    newProduct.customTextCharLimit2=data[item].customTextCharLimit2;
-                    newProduct.customTextField1=data[item].customTextField1;
-                    newProduct.customTextField2=data[item].customTextField2;
-                    newProduct.customTextMandatory1=data[item].customTextMandatory1;
-                    newProduct.customTextMandatory2=data[item].customTextMandatory2;
-                    newProduct.description=data[item].description;
-                    newProduct.discountMode=data[item].discountMode;
-                    newProduct.discountValue=data[item].discountValue;
+
+                    newProduct.handleId=data[item].handleId;
                     newProduct.fieldType=data[item].fieldType;
-                    newProduct.handleIds=handleId;
-                    newProduct.inventory=data[item].inventory;
                     newProduct.name=data[item].name + ' ' + collectionsArray[collections];
-                    newProduct.price=price;
+                    newProduct.description=productSizeObj[collectionsArray[collections]]=="One Size fits most" ? data[item].description + " One Size fits most.":data[item].description;
                     newProduct.productImageUrl=data[item].productImageUrl;
-                    newProduct.productOptionDescription1=productSizeObj[collectionsArray[0]];
-                    newProduct.productOptionDescription2='trash';
-                    newProduct.productOptionDescription3='trash';
-                    newProduct.productOptionDescription4='trash';
-                    newProduct.productOptionDescription5='trash';
-                    newProduct.productOptionDescription6='trash';
-                    newProduct.productOptionName1='Sizes';
-                    newProduct.productOptionName2='trash';
-                    newProduct.productOptionName3='trash';
-                    newProduct.productOptionName4='trash';
-                    newProduct.productOptionName5='trash';
-                    newProduct.productOptionName6='trash';
-                    newProduct.productOptionType1='DROP_DOWN';
-                    newProduct.productOptionType2='trash';
-                    newProduct.productOptionType3='trash';
-                    newProduct.productOptionType4='trash';
-                    newProduct.productOptionType5='trash';
-                    newProduct.productOptionType6='trash';
-                    newProduct.ribbon=data[item].ribbon;
+                    newProduct.collections=collectionsArray[0];
                     newProduct.sku=data[item].sku;
+                    newProduct.ribbon=data[item].ribbon;
+                    newProduct.price=data[item].price;
                     newProduct.surcharge=data[item].surcharge;
                     newProduct.visible=data[item].visible;
+                    newProduct.discountMode=data[item].discountMode;
+                    newProduct.discountValue=data[item].discountValue;
+                    newProduct.inventory=data[item].inventory;
                     newProduct.weight=data[item].weight;
+                    newProduct.productOptionName1=productSizeObj[collectionsArray[collections]]=="One Size fits most" || collectionsArray[collections]=='Scrub Cap' || collectionsArray[collections]=='Scrub Caps' || collectionsArray[collections]=='Bouffant Scrub Cap'|| collectionsArray[collections]=='Long Hair Scrub Cap'?'trash':'Sizes';
+                    newProduct.productOptionType1=productSizeObj[collectionsArray[collections]]=="One Size fits most" || collectionsArray[collections]=='Scrub Cap' || collectionsArray[collections]=='Scrub Caps' || collectionsArray[collections]=='Bouffant Scrub Cap'|| collectionsArray[collections]=='Long Hair Scrub Cap'?'trash':'DROP_DOWN';
+                    newProduct.productOptionDescription1=productSizeObj[collectionsArray[0]]=="One Size fits most"?'trash':productSizeObj[collectionsArray[0]];
+                    newProduct.productOptionName2="trash";
+                    newProduct.productOptionType2="trash";
+                    newProduct.productOptionDescription2="trash";
+                    newProduct.productOptionName3="trash";
+                    newProduct.productOptionType3="trash";
+                    newProduct.productOptionDescription3="trash";
+                    newProduct.productOptionName4="trash";
+                    newProduct.productOptionType4="trash";
+                    newProduct.productOptionDescription4="trash";
+                    newProduct.productOptionName5="trash";
+                    newProduct.productOptionType5="trash";
+                    newProduct.productOptionDescription5="trash";
+                    newProduct.productOptionName6="trash";
+                    newProduct.productOptionType6="trash";
+                    newProduct.productOptionDescription6="trash";
+                    newProduct.additionalInfoTitle1="trash";
+                    newProduct.additionalInfoDescription1="trash";
+                    newProduct.additionalInfoTitle2="trash";
+                    newProduct.additionalInfoDescription2="trash";
+                    newProduct.additionalInfoTitle3="trash";
+                    newProduct.additionalInfoDescription3="trash";
+                    newProduct.additionalInfoTitle4="trash";
+                    newProduct.additionalInfoDescription4="trash";
+                    newProduct.additionalInfoTitle5="trash";
+                    newProduct.additionalInfoDescription5="trash";
+                    newProduct.additionalInfoTitle6="trash";
+                    newProduct.additionalInfoDescription6="trash";
+                    newProduct.customTextField1=data[item].customTextField1;
+                    newProduct.customTextCharLimit1=data[item].customTextCharLimit1;
+                    newProduct.customTextMandatory1=data[item].customTextMandatory1;
+                    newProduct.customTextField2=data[item].customTextField2;
+                    newProduct.customTextCharLimit2=data[item].customTextCharLimit2;
+                    newProduct.customTextMandatory2=data[item].customTextMandatory2;
 
                     newData.push(newProduct);
 
